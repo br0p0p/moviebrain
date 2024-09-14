dffa687688c4813d8c47bbfc5235e78613082f9f
Caleb Williams <br0p0p@users.noreply.github.com>
2024-09-13 23:29:37 -0500

    Fleshing out templates, get search kinda working, use PORT env var

--- server.go
+++ server.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"io"
+	"log"
 	"net/http"
+	"os"
 	"text/template"
 
 	"github.com/labstack/echo/v4"
@@ -10,18 +12,30 @@ import (
 
 func main() {
 	e := echo.New()
+	e.Static("/assets", "assets")
 
-	t := &Template{
+	tmpl := &Template{
 		templates: template.Must(template.ParseGlob("public/views/*.html")),
 	}
 
-	e.Renderer = t
+	e.Renderer = tmpl
 
 	e.GET("/", Index)
-
 	e.GET("/hello", Hello)
+	e.GET("/search", Search)
 
-	e.Logger.Fatal(e.Start(":1323"))
+	addr := ":" + getPort()
+	e.Logger.Fatal(e.Start(addr))
+}
+
+func getPort() string {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("Please specify the HTTP port as environment variable, e.g. env PORT=8081 go run http-server.go")
+	}
+
+	return port
 }
 
 type (
@@ -43,9 +57,33 @@ func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Con
 }
 
 func Index(c echo.Context) error {
-	return c.Render(http.StatusOK, "index", "World")
+	return c.Render(http.StatusOK, "base", "World")
 }
 
 func Hello(c echo.Context) error {
 	return c.Render(http.StatusOK, "hello", "World")
+}
+
+type User struct {
+	Name  string `json:"name" xml:"name"`
+	Email string `json:"email" xml:"email"`
+}
+
+func Search(c echo.Context) error {
+	query := c.QueryParam("q")
+
+	// Example movie data
+	movies := []map[string]interface{}{
+		{"Title": "The Shawshank Redemption", "Director": "Frank Darabont", "Year": 1994},
+		{"Title": "The Godfather", "Director": "Francis Ford Coppola", "Year": 1972},
+		{"Title": "The Dark Knight", "Director": "Christopher Nolan", "Year": 2008},
+	}
+
+	// Pass the movies list to the template
+	data := map[string]interface{}{
+		"Query":  query,
+		"Movies": movies,
+	}
+
+	return c.Render(http.StatusOK, "movies", data)
 }
