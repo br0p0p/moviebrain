8b3d1408406d71537647e38106bfe4debfb33e0d
Caleb Williams <br0p0p@users.noreply.github.com>
2024-09-17 00:08:40 -0500

    Search is roughly working

--- server.go
+++ server.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"os"
 	"text/template"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/lib/pq"
 )
 
+var db *sqlx.DB
+
 func main() {
+	db = sqlx.MustConnect("postgres", os.Getenv("DATABASE_URL"))
+
 	e := echo.New()
+	e.Logger.SetLevel(log.DEBUG)
 	e.Static("/assets", "assets")
 
 	tmpl := &Template{
@@ -64,26 +74,82 @@ func Hello(c echo.Context) error {
 	return c.Render(http.StatusOK, "hello", "World")
 }
 
-type User struct {
-	Name  string `json:"name" xml:"name"`
-	Email string `json:"email" xml:"email"`
+type Movie struct {
+	BackdropPath  string  `db:"backdrop_path"`
+	ID            int     `db:"id"`
+	Title         string  `db:"title"`
+	OriginalTitle string  `db:"original_title"`
+	Overview      string  `db:"overview"`
+	PosterPath    string  `db:"poster_path"`
+	Popularity    float64 `db:"popularity"`
+	ReleaseDate   string  `db:"release_date"`
+}
+
+type QueryResults struct {
+	Query  string
+	Movies []Movie
 }
 
 func Search(c echo.Context) error {
 	query := c.QueryParam("q")
 
-	// Example movie data
-	movies := []map[string]interface{}{
-		{"Title": "The Shawshank Redemption", "Director": "Frank Darabont", "Year": 1994},
-		{"Title": "The Godfather", "Director": "Francis Ford Coppola", "Year": 1972},
-		{"Title": "The Dark Knight", "Director": "Christopher Nolan", "Year": 2008},
+	if len(query) <= 1 {
+		return c.Render(http.StatusOK, "no_results", nil)
 	}
+
+	searchTerm := "%" + query + "%"
+	// movies := []Movie{}
+
+	fmt.Println("Searching for", searchTerm)
+
+	// err := db.Select(&movies, "SELECT * FROM movie WHERE to_tsvector(title) @@ to_tsquery($1)", query)
+	rows, err := db.Queryx("SELECT * FROM movie WHERE title LIKE $1", searchTerm)
+	defer rows.Close()
+
+	movies := []map[string]interface{}{}
+
+	// for rows.Next() {
+	// 	err = rows.MapScan(movies)
+	// 	fmt.Printf("results: %v\n\n", movies)
+	// }
+
+	for rows.Next() {
+		// Create a map to hold the row data
+		rowData := map[string]interface{}{}
+
+		// Use MapScan to scan the row into the map
+		if err := rows.MapScan(rowData); err != nil {
+			log.Fatal(err)
+		}
+
+		// Append the map to the results slice
+		movies = append(movies, rowData)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("movies: %v\n\n", movies)
+
+	// Example movie data
+	// _movies2 := []map[string]interface{}{
+	// 	{"Title": "The Shawshank Redemption", "Director": "Frank Darabont", "Year": 1994},
+	// 	{"Title": "The Godfather", "Director": "Francis Ford Coppola", "Year": 1972},
+	// 	{"Title": "The Dark Knight", "Director": "Christopher Nolan", "Year": 2008},
+	// }
 
 	// Pass the movies list to the template
 	data := map[string]interface{}{
 		"Query":  query,
 		"Movies": movies,
 	}
+
+	// b, err := json.Marshal(data)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Println("JSON:", string(b))
 
 	return c.Render(http.StatusOK, "movies", data)
 }
